53e6de65558b2305b3ee074367604dcd74063f18
Mukul Hase <mukulhase@gmail.com>
2017-08-06 13:23:58 +0530

    Added starter code and library structure

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## 3-Sweep implementation in Python
+- Install instructions:
+    - Create Virtual env
+    - `pip install requirements.txt`
+    - `cd venv/lib/python2.7/site-packages/`
+    - `ln -s /usr/local/lib/python2.7/site-packages/cv.py cv.py`
+    - `ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so`
---
+++ ThreeSweep.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+
+class ThreeSweep():
+    ''' Module class for Three Sweep '''
+
+    def __init__(self):
+        self.image = None
+        self.contourPoints = []
+        self.objectPoints = []
+        pass
+
+    def loadImage(self, filename):
+        ''' Load image into module for processing '''
+        self.image = cv2.imread(filename, 0)
+        pass
+
+    def getEdges(self):
+        ''' Run edge detection on the image '''
+        pass
+
+    def setMajor(self):
+        ''' Set points for Major Axis '''
+        pass
+
+    def addSweepPoint(self):
+        ''' Called everytime another point on the axis is given by user '''
+
+        def detectBoundaryPoints(axisPoint, slope):
+            ''' Detect points on the boundary '''
+            pass
+
+    def createSTL(self):
+        ''' takes the object points in order and creates pairs of triangles, writes to a file'''
+        pass
