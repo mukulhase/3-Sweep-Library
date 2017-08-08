ee97ca6bd4ad17da9c4c891973bb92e2726addb6
Mukul Hase <mukulhase@gmail.com>
2017-08-09 01:57:38 +0530

    Primitive and Plot Code

--- ThreeSweep.py
+++ ThreeSweep.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class ThreeSweep():
     ''' Module class for Three Sweep '''
@@ -10,6 +11,7 @@ class ThreeSweep():
         self.contourPoints = []
         self.objectPoints = []
         self.axisResolution = 10
+        self.primitiveDensity = 40
         pass
 
     def loadImage(self, filename):
@@ -31,6 +33,18 @@ class ThreeSweep():
         def detectBoundaryPoints(axisPoint, slope):
             ''' Detect points on the boundary '''
             pass
+
+    def pickPrimitive(self):
+        ''' To select whether shape will be a circle or square(will be automated in the future) '''
+        self.primitiveDensity = 40
+        angles = np.linspace(0, 2 * np.pi, self.primitiveDensity)
+        return np.array([np.cos(angles), np.sin(angles), np.zeros(self.primitiveDensity)])
+
+    def plot3DArray(self,x):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x[0], x[1], x[2])
+        plt.show()
 
     def createSTL(self):
         ''' takes the object points in order and creates pairs of triangles, writes to a file'''
